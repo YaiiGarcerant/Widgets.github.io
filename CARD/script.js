329b2b092af2c4b2bf12c65193c20ce611065e67
YaiiGarcerant <126992301+YaiiGarcerant@users.noreply.github.com>
2026-02-05 10:24:47 -0500

    progress var

--- CARD/script.js
+++ CARD/script.js
@@ -1,29 +1,52 @@
 const wrapper = document.getElementById('wrapper');
 const panels = document.querySelectorAll('.panel');
 const banner = document.getElementById('banner');
+const progressWrapper = document.getElementById('progress-wrapper');
+const progressBar = document.getElementById('progress-bar');
 
 let currentIndex = 0;
 let locked = false;
-const ANIMATION_TIME = .8;
+
+const ANIMATION_TIME = .5;
 const SWIPE_THRESHOLD = 60;
 
 let startX = 0;
 let startY = 0;
 
-/* Sincronizar layout */
+const progressSteps = document.getElementById('progress-steps');
+
+/* Crear segmentos */
+panels.forEach(() => {
+  const step = document.createElement('div');
+  step.className = 'progress-step';
+  progressSteps.appendChild(step);
+});
+
+const steps = document.querySelectorAll('.progress-step');
+
+
+
 function updateLayout() {
+  steps.forEach((step, i) => {
+    step.classList.toggle('active', i === currentIndex);
+  });
+
   if (currentIndex === 0) {
     banner.classList.remove('visible');
+    progressWrapper.classList.remove('below-banner');
     panels.forEach(p => p.classList.remove('with-banner'));
   } else {
     banner.classList.add('visible');
+    progressWrapper.classList.add('below-banner');
     panels.forEach((p, i) => {
       if (i > 0) p.classList.add('with-banner');
     });
   }
 }
 
-/* Movimiento 1 sección */
+
+
+/* Movimiento controlado */
 function move(direction) {
   if (locked) return;
 
@@ -33,7 +56,6 @@ function move(direction) {
   locked = true;
   currentIndex = next;
 
-  // 🔥 TODO se mueve junto
   updateLayout();
   wrapper.style.transform = `translateX(-${currentIndex * 100}vw)`;
 
